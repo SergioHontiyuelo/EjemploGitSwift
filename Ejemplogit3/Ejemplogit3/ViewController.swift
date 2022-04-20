b1e6d7a7fd89c05a5eb1beea24238f376a00512a
SergioHontiyuelo <serhony@gmail.com>
2022-04-20 14:10:24 -0700

    feat: Added new print

---
+++ Ejemplogit3/Ejemplogit3/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Ejemplogit3
+//
+//  Created by Alumno Swift on 20/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        print("hola :D")
+        
+        
+        
+    }
+
+    
+}
+
+
